6b47ac9036dcde04a729ee83d99c5e439fe9b4d5
sofienesdiri <sofiene.sdiri5@gmail.com>
2022-04-26 15:36:25 +0200

    vfvd

--- Middlewares/Upload.js
+++ Middlewares/Upload.js
@@ -1,7 +1,7 @@
 const multer = require('multer')
 const storage = multer.diskStorage({
     destination: function (req, file, cb) {
-      cb(null, './client/public/images')
+      cb(null, './upload')
     },
     filename: function (req, file, cb) {
       
